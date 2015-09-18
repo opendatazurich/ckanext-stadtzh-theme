4f45efb1cd7be0f6c3475d6dc6e9db5dcfd55040
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2015-09-18 17:57:56 +0200

    Read YAML config and display resource description accordingly

--- ckanext/stadtzhtheme/plugin.py
+++ ckanext/stadtzhtheme/plugin.py
@@ -2,6 +2,7 @@
 
 import logging
 import json
+import yaml
 import re
 import urlparse
 import lepl.apps.rfc3696
@@ -217,19 +218,12 @@ class StadtzhThemePlugin(plugins.SingletonPlugin,
     plugins.implements(plugins.IDatasetForm, inherit=False)
     plugins.implements(plugins.ITemplateHelpers, inherit=False)
 
-    # These record how many times methods that this plugin's methods are
-    # called, for testing purposes.
-    num_times_new_template_called = 0
-    num_times_read_template_called = 0
-    num_times_edit_template_called = 0
-    num_times_comments_template_called = 0
-    num_times_search_template_called = 0
-    num_times_history_template_called = 0
-    num_times_package_form_called = 0
-    num_times_check_data_dict_called = 0
-    num_times_setup_template_variables_called = 0
-
     def update_config(self, config):
+        try:
+            with open(config.get('ckanext.stadtzh-theme.descr_file'), 'r') as descr_file:
+                self.descr_config = yaml.load(descr_file))
+        except IOError:
+            self.descr_config = {}
 
         tk.add_template_directory(config, 'templates')
         tk.add_public_directory(config, 'public')
@@ -430,46 +424,7 @@ class StadtzhThemePlugin(plugins.SingletonPlugin,
 
         return schema
 
-    # These methods just record how many times they're called, for testing
-    # purposes.
-    # TODO: It might be better to test that custom templates returned by
-    # these methods are actually used, not just that the methods get
-    # called.
-
     def setup_template_variables(self, context, data_dict):
-        StadtzhThemePlugin.num_times_setup_template_variables_called += 1
+        context['descr_config'] = self.descr_config
         return super(StadtzhThemePlugin, self).setup_template_variables(
             context, data_dict)
-
-    def new_template(self):
-        StadtzhThemePlugin.num_times_new_template_called += 1
-        return super(StadtzhThemePlugin, self).new_template()
-
-    def read_template(self):
-        StadtzhThemePlugin.num_times_read_template_called += 1
-        return super(StadtzhThemePlugin, self).read_template()
-
-    def edit_template(self):
-        StadtzhThemePlugin.num_times_edit_template_called += 1
-        return super(StadtzhThemePlugin, self).edit_template()
-
-    def comments_template(self):
-        StadtzhThemePlugin.num_times_comments_template_called += 1
-        return super(StadtzhThemePlugin, self).comments_template()
-
-    def search_template(self):
-        StadtzhThemePlugin.num_times_search_template_called += 1
-        return super(StadtzhThemePlugin, self).search_template()
-
-    def history_template(self):
-        StadtzhThemePlugin.num_times_history_template_called += 1
-        return super(StadtzhThemePlugin, self).history_template()
-
-    def package_form(self):
-        StadtzhThemePlugin.num_times_package_form_called += 1
-        return super(StadtzhThemePlugin, self).package_form()
-
-    # check_data_dict() is deprecated, this method is only here to test that
-    # legacy support for the deprecated method works.
-    def check_data_dict(self, data_dict, schema=None):
-        StadtzhThemePlugin.num_times_check_data_dict_called += 1
