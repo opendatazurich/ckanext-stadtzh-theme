6f3088f9a6c42d4db17a2cf6eab4e1e5d2a4f516
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2018-03-01 10:22:07 +0100

    Add tests to plugin

---
+++ bin/travis-run.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+set -e
+
+function cleanup {
+    exit $?
+}
+
+trap "cleanup" EXIT
+
+# Check PEP-8 code style and McCabe complexity
+flake8 --statistics --show-source ckanext
+
+# run tests
+nosetests --ckan --nologcapture --with-pylons=subdir/test.ini --verbose ckanext/stadtzhtheme
---
+++ ckanext/stadtzhtheme/tests/test_plugin.py
@@ -0,0 +1,21 @@
+import nose
+from ckan.tests import helpers
+
+import ckanext.stadtzhtheme.plugin as plugin
+
+eq_ = nose.tools.eq_
+assert_true = nose.tools.assert_true
+
+
+class TestPlugin(helpers.FunctionalTestBase):
+    def setUp(self):
+        return
+
+    def tearDown(self):
+        return
+
+    def test_descr_file(self):
+        theme_plugin = plugin.StadzhThemePlugin()
+
+        descr = theme_plugin.get_descr_config()
+        eq_(desc, {})
