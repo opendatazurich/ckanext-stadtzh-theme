455684a4f5913cb56de4b41f1b341d0ba8cad2c3
Stefan Oderbolz <stefan.oderbolz@liip.ch>
2018-10-18 14:00:56 +0200

    Read resource description from descr file if needed

--- ckanext/stadtzhtheme/tests/test_plugin.py
+++ ckanext/stadtzhtheme/tests/test_plugin.py
@@ -14,8 +14,23 @@ class TestPlugin(helpers.FunctionalTestBase):
         theme_plugin = plugin.StadtzhThemePlugin()
 
         expected_keys = ['zip', 'wms', 'wmts', 'wfs', 'kml', 'kmz', 'json', 'csv', 'gpkg'] 
-        descr = theme_plugin.get_descr_config()
+        descr = theme_plugin.descr_config
         assert all(k in descr.keys() for k in expected_keys), "Keys: %s" % descr.keys()
+
+    def test_resource_description_value(self):
+        theme_plugin = plugin.StadtzhThemePlugin()
+        res = {'format': 'CSV', 'description': 'My super CSV'} 
+        eq_(theme_plugin.get_resource_descriptions(res), ['My super CSV'])
+
+    def test_resource_description_file(self):
+        theme_plugin = plugin.StadtzhThemePlugin()
+        res = {'format': 'CSV', 'description': ''} 
+
+        descr = theme_plugin.get_resource_descriptions(res)
+
+        eq_(len(descr), 2)
+        eq_(descr[0], 'Comma-Separated Values.') 
+        eq_(descr[1], 'Weitere Informationen zu CSV finden Sie in unserer Rubrik Werkstatt unter [Informationen zu Datenformaten.](https://www.stadt-zuerich.ch/portal/de/index/ogd/werkstatt/csv.html)') 
 
     def test_translations_without_orgs(self):
         dataset = factories.Dataset()
