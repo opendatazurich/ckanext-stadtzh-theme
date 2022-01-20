471fc55cd7dc968a9891b571aad5bf745a52fd01
Rae Knowler <rae.knowler@liip.ch>
2022-01-20 16:29:55 +0100

    Add extra test for resource url validator

--- ckanext/stadtzhtheme/tests/test_validation.py
+++ ckanext/stadtzhtheme/tests/test_validation.py
@@ -9,6 +9,8 @@ assert_true = nose.tools.assert_true
 class TestValidation(helpers.FunctionalTestBase):
 
     def test_invalid_url(self):
+        """Test that an invalid resource url is caught by our validator.
+        """
         factories.Sysadmin(apikey="my-test-key")
         app = self._get_test_app()
         demo = TestAppCKAN(app, apikey="my-test-key")
@@ -27,3 +29,22 @@ class TestValidation(helpers.FunctionalTestBase):
             )
         else:
             raise AssertionError('ValidationError not raised')
+
+    def test_invalid_url_for_upload_resource_type(self):
+        """Test that the resource url is not validated if the url_type
+        is 'upload'.
+        """
+        factories.Sysadmin(apikey="my-test-key")
+        app = self._get_test_app()
+        demo = TestAppCKAN(app, apikey="my-test-key")
+
+        try:
+            dataset = factories.Dataset()
+            demo.action.resource_create(
+                package_id=dataset['name'],
+                name='Test-File',
+                url='https://example.com]',
+                url_type='upload'
+            )
+        except ValidationError as e:
+            raise AssertionError('ValidationError raised erroneously')
